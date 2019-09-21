2cd74194b39ab51be4077ac4b33388ef470f962f
YianMue <dinpally@gmail.com>
2019-09-21 11:17:35 -0700

    Add project files.

---
+++ CarpetCleanup/CarpetCleanup.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	int smallRooms;
+	int largeRooms;
+	const double smallRoomPrice{ 25 };
+	const double largeRoomPrice{ 35 };
+	const double taxRate{ .06 };
+	const double estimateExpiry{ 30 };
+
+	cout << "Please enter number of small rooms: " << endl;
+	cin >> smallRooms;
+
+	return 0;
+}
